19f41fd5b5b4f18aae6d36e2eecdd3d4b6e2a979
Roman <roman@macbook.local>
2024-01-31 16:20:42 +0200

    Added footer text

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
           <NavBar>
             <template v-slot:navbar-brand>
               <a class="navbar-brand ms-sm-3" href="#">
-                <img class="logo" src="assets/Logo_meleshko.jpeg" width="35" height="35" alt=""/>
+                <img class="logo" src="assets/Icon.png" width="35" height="35" alt=""/>
               </a>
             </template>
             <template v-slot:navbar-items>
@@ -30,7 +30,54 @@
 
         <footer>
 
-          <Footer></Footer>
+          <Footer>
+            <template v-slot:footer-banner>
+
+              <img src="assets/Label-Meleshko.png" width="150" alt="">
+
+            </template>
+            <template v-slot:footer-place>
+
+              <ul class="ps-0" v-for="item in location" :key="item.id">
+                <li class="footer_title pb-2">{{ item.title }}</li>
+                <li class="contacts">
+                  <span>г. </span>
+                  <span>{{ item.place }}</span>
+                </li>
+              </ul>
+
+            </template>
+            <template v-slot:footer-callback>
+
+              <ul class="ps-0 my-sm-0" v-for="item in callback" :key="item.id">
+                <li class="footer_title pb-2">{{ item.title }}</li>
+                <li class="contacts">
+                  <span>тел: </span>
+                  <span>{{ item.phone }}</span>
+                </li>
+                <li class="contacts">
+                  <span>email: </span>
+                  <span>{{ item.email }}</span>
+                </li>
+              </ul>
+
+            </template>
+            <template v-slot:footer-call>
+
+              <ul class="ps-0 my-sm-0" v-for="item in icons" :key="item.id">
+                <li class="footer_title pb-2">{{ item.title }}</li>
+                <li>
+                  <span class="icon px-1">
+                    <a v-for="point in item.links" :href="point.link" target="_blank">
+                      <font-awesome-icon :icon="point.icon" />
+                    </a>
+                  </span>
+                </li>
+              </ul>
+
+            </template>
+
+          </Footer>
 
         </footer>
 
@@ -44,12 +91,13 @@
 <script lang="ts">
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-import { defineComponent } from 'vue'
+import { defineComponent, defineAsyncComponent } from 'vue'
 
 import Note from "models/interface/Note";
+import Link from "models/interface/Link";
 
 import NavBar from "components/NavBar/NavBar.vue";
-import Footer from  "components/Footer/Footer.vue";
+// import Footer from  "components/Footer/Footer.vue";
 
 export default defineComponent({
   name: "App",
@@ -69,13 +117,52 @@ export default defineComponent({
           id: 3,
           name: "Contact",
         } as Note
-      ]
+      ],
+      location: [
+        {
+          id: 1,
+          title: "Местонахождение",
+          place: "Полтава",
+        } as Note
+      ],
+      callback: [
+        {
+          id: 1,
+          title: "Обратная связь",
+          phone: "0955209822",
+          email: "roman.meleshko1@gmail.com"
+        } as Note
+      ],
+      icons: [
+        {
+          id: 1,
+          title: "Связь",
+          links: [
+            {
+              link: "https://github.com/RomanMeleshko/MeleshkoCV",
+              icon: "fa-brands fa-github",
+            } as Link,
+            {
+              link: "https://www.instagram.com/roma_meleshko/",
+              icon: "fa-brands fa-square-instagram"
+            } as Link,
+            {
+              link: "https://www.linkedin.com/in/roman-meleshko-400036160/",
+              icon: "fa-brands fa-linkedin"
+            } as Link,
+          ],
+        } as Note,
 
+      ],
     }
   },
   components: {
     NavBar,
-    Footer
+    Footer: defineAsyncComponent({
+      loader: () => import("components/Footer/Footer.vue"),
+      delay: 1000,
+      timeout: 3000
+    })
   }
 })
 
@@ -85,6 +172,14 @@ export default defineComponent({
 <style lang="scss">
 @import "scss/app.scss";
 @import "scss/variables.scss";
+
+ // Header
+
+ header {
+   position: sticky;
+   top: 0px;
+   z-index: 999;
+ }
 
  .logo {
    border-radius: 50%;
@@ -100,6 +195,44 @@ export default defineComponent({
      margin-top: 5px;
      font-size: 12px;
    }
+ }
+
+ // Footer
+ ul {
+   list-style-type: none;
+ }
+
+ .contacts > span:first-child {
+   color: #9fa19e;
+ }
+
+ .footer_title {
+   font-size: clamp(14px, 2.5vw, 16px);
+ }
+
+ .contacts {
+   font-size: clamp(12px, 2.5vw, 14px);
+ }
+
+ .fa-github,
+ .fa-square-instagram,
+ .fa-linkedin {
+   width: 30px;
+   height: 30px;
+   color: #eece49;
+   border-radius: 7px;
+   box-shadow: 2px 9px 12px #ECEDED;
+   padding: 5px;
+   //transform: rotate(0deg);
+   transition: all 0.3s linear 0s;
+
+   &:hover {
+     transform: scale(1.2);
+   }
+ }
+
+ .footer_call {
+   min-width: 140px;
  }
 
 </style>
